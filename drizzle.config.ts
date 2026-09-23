0a28e51cacaef715c45f809c566c94d0d76f3a08
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2026-09-23 12:13:03 +0800

    Connected to a Turso SQLite database for Vercel

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,15 @@
 // drizzle.config.ts
 import { defineConfig } from "drizzle-kit";
+import { tursoUrl } from "./src/db";
 
 export default defineConfig({
 	schema: "./src/schema.ts",
 	out: "./drizzle",
-	dialect: "sqlite",
+	// dialect: "sqlite",
+	dialect: "turso",
 	dbCredentials: {
-		url: "sqlite.db",
+		// url: "sqlite.db",
+		url: tursoUrl,
+		authToken: process.env.TURSO_AUTH_TOKEN,
 	},
 });
--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,24 @@
 // src/db.ts
-import { Database } from "bun:sqlite";
-import { drizzle } from "drizzle-orm/bun-sqlite";
+// import { Database } from "bun:sqlite"; // Commented in case it is needed for local testing
+import { createClient } from "@libsql/client";
+// import { drizzle as drizzleBun } from "drizzle-orm/bun-sqlite"; // Commented in case it is needed for local testing
+import { drizzle as drizzleLibsql } from "drizzle-orm/libsql";
 
-// This will automatically create a local "sqlite.db" file in your project root
-const sqlite = new Database("sqlite.db");
+export const tursoUrl = (() => {
+	const tursoUrl = process.env.TURSO_DATABASE_URL;
 
-export const db = drizzle({ client: sqlite });
+	if (!tursoUrl) {
+		throw new Error(
+			"❌ TURSO_DATABASE_URL is missing from your environment variables. Please check your .env file or Vercel dashboard.",
+		);
+	}
+	return tursoUrl;
+})();
+
+export const db = drizzleLibsql({
+	client: createClient({
+		url: tursoUrl,
+		authToken: process.env.TURSO_AUTH_TOKEN,
+	}),
+});
+// export const db = drizzleBun({ client: new Database("sqlite.db") }); // Commented in case it is needed for local testing
